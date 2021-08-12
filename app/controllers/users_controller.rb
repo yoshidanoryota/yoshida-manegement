6001c765e769beaa8d6d2ea15a3a906ed9b9ab11
yoshidanoryota <yoshidanoryota777x@gmail.com>
2021-08-12 12:37:49 +0900

    サインアップ・ログイン設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "登録に成功しました"
+    else
+      redirect_to new_user_path, notice: "入力に不備があります"
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name,:number, :division_id, :office_id)
+  end
+end
